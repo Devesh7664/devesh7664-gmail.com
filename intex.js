0ea049d3dc18811d7aaf5f978008ca80667ceafe
Devesh7664 <devesh7664@gmail.com>
2023-09-27 11:25:36 +0530

    chrome

---
+++ intex.js
@@ -0,0 +1,8 @@
+
+//access karne ke liye 2 notation use kar skte hau person.age or person['age'];
+ //array let name=['shiva','devesh','sani']
+//js basic ]
+//lconsole.log(5 !== 3);
+let age = 17;
+let status =(age >=18 )? 'yes vote': 'no vote';
+console.log(status);
